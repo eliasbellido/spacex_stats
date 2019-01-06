89d7c77d39748ea2d89307d533c89b62b45dec37
eliasbtc <ebellido6@gmail.com>
2019-01-06 01:26:09 -0500

    Fragments used, finishing app

--- client/src/components/Launches.js
+++ client/src/components/Launches.js
@@ -2,7 +2,7 @@ import React, { Component, Fragment } from 'react';
 import gql from 'graphql-tag';
 import { Query } from 'react-apollo';
 import LaunchItem from './LaunchItem';
-
+import MissionKey from './MissionKey';
 const LAUNCHES_QUERY = gql`
     query LaunchesQuery{
         launches {
@@ -17,7 +17,8 @@ export class Launches extends Component {
   render() {
     return (
       <Fragment>
-        <h1 className="display-4 my-3">Launches</h1>
+        <h1 className="display-4 my-3">Lanzamientos</h1>
+        <MissionKey/>
         <Query query={LAUNCHES_QUERY}>
             {
                 ({ loading, error, data }) => {
